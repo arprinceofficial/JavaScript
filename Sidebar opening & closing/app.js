df9956573d81bd58afef37b4e65cd65dcf389c3f
Md. Ashqiur Rahman <rashiqur92@gmail.com>
2021-11-11 11:29:17 +0600

    first commit slide-bar using css javascript

---
+++ Sidebar opening & closing/app.js
@@ -0,0 +1,31 @@
+// const toggle = document.getElementById('toggle');
+// console.log('Toggle: ', toggle);
+// console.log('Document Ready State', document.readyState);
+// toggle.onclick = function() {
+//     toggle.classList.toggle('active');
+// }
+
+// const toggle = document.getElementById('toggle');
+// const sidebar = document.getElementById('sidebar');
+
+// document.onclick = function(e) {
+//     if (e.target.id !== 'sidebar' && e.target.id !== 'toggle') {
+//         toggle.classList.remove('active');
+//         sidebar.classList.remove('active');
+//     }
+// }
+
+// toggle.onclick = function() {
+//     toggle.classList.toggle('active');
+//     sidebar.classList.toggle('active');
+// }
+
+let toggleBtn = document.querySelector('#toggle');
+
+function toggleSidebar() {
+    let sideBarNode = document.querySelector('#sidebar');
+    sideBarNode.classList.toggle('show');
+
+}
+
+toggleBtn.addEventListener('click', toggleSidebar);
